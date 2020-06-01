629b3d5b2cb602d8e5d3ede1a7513df28678047a
Hilal Güvenç <hilalguvenc@yahoo.com>
2020-06-01 18:56:51 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div >
+    <div>
+      <div className="editor-wrap">
+        <div className="toolbar">
+          <i className="fab fa-free-code-camp"></i>
+          Editor
+          <i className="fa fa-arrows-alt"></i>
+        </div>
+        <textarea id="editor" type="text"></textarea>
+      </div>
+      <div className="preview-wrap">
+        <div className="toolbar">
+        <i className="fab fa-free-code-camp"></i>
+          Previewer
+          <i className="fa fa-arrows-alt"></i>
+        </div>
+        <textarea id="preview" type="text"></textarea>
+      </div>
     </div>
   );
 }
-
 export default App;
